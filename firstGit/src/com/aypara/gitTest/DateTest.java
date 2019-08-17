9f6563e6c3807bd85dfd8945958f1ca3d82276d7
saeed <leysi.saeed@gmail.com>
2019-08-17 23:10:53 +0430

    printing date.

--- firstGit/src/com/aypara/gitTest/DateTest.java
+++ firstGit/src/com/aypara/gitTest/DateTest.java
@@ -1,10 +1,12 @@
 package com.aypara.gitTest;
 
+import java.util.Date;
+
 public class DateTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(new Date().getDate());
 	}
 
 }
